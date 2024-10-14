4ebcbd2900210546549df21b42ee8b9defeab343
VinhCanCode <sandilove59@gmail.com>
2024-10-15 01:42:31 +0700

    update new table for payments

--- Frontend/Koi-Care-System/src/pages/Shop/Payment/Payment.jsx
+++ Frontend/Koi-Care-System/src/pages/Shop/Payment/Payment.jsx
@@ -13,13 +13,16 @@ import { FaCartArrowDown } from "react-icons/fa";
 import { FaMoneyBillWave } from "react-icons/fa";
 import { GrNotes } from "react-icons/gr";
 import { MdPendingActions } from "react-icons/md";
+import * as XLSX from 'xlsx'
+import { DataGrid } from '@mui/x-data-grid'
+import Paper from '@mui/material/Paper'
 
 function Payment() {
   const { isDarkMode } = useDarkMode()
   const [payments, setPayments] = useState([])
   const [selectedOrder, setSelectedOrder] = useState(null)
   const [isModalOpen, setIsModalOpen] = useState(false)
-  // const [showButtons, setShowButtons] = useState(false)
+
   const formatDateTime = (inputDate) => {
     const date = new Date(inputDate);
     const day = String(date.getDate()).padStart(2, '0')
@@ -28,11 +31,21 @@ function Payment() {
     const hours = String(date.getHours()).padStart(2, '0')
     const minutes = String(date.getMinutes()).padStart(2, '0')
     const seconds = String(date.getSeconds()).padStart(2, '0')
-    return `${month}/${day}/${year} ${hours}:${minutes}:${seconds}`
+    return `${hours}:${minutes}:${seconds} ${day}/${month}/${year}`
+  }
+
+  const getStatusColor = (status) => {
+    switch (status) {
+      case 'COMPLETED':
+        return 'text-white bg-green-500 '
+      case 'CANCELLED':
+        return 'text-red-500 bg-red-200'
+      default:
+        return 'text-gray-500 bg-gray-200'
+    }
   }
 
   const formatCurrency = (amount) => amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + "đ"
-
 
   const handleCloseModal = () => {
     setIsModalOpen(false)
@@ -81,8 +94,47 @@ function Payment() {
     }
   };
 
-
-
+  const columns = [
+    { field: 'invoiceCode', headerName: 'Invoice Code', width: 200 },
+    { field: 'createDate', headerName: 'Create Date', flex : 1, renderCell: (params) => formatDateTime(params.row.createDate) },
+    { field: 'transactionCode', headerName: 'Transaction Code', width: 200 },
+    
+    { field: 'amount', headerName: 'Amount', flex : 1, renderCell: (params) => formatCurrency(params.row.amount) },
+    {
+      field: 'status',
+      headerName: 'Status',
+      width: 200,
+      renderCell: (params) => {
+        const status = params.value; 
+        const isCompleted = status === 'COMPLETED';
+        
+        return (
+          <div className='h-full flex justify-center items-center'>
+            <div
+              className={`flex items-center justify-center py-1 px-2 rounded text-sm font-medium
+                ${isCompleted ? 'border-2 border-green-500 text-green-500' : 'border-2 border-red-500 text-red-500'}`}
+            >
+              {isCompleted ? 'COMPLETED' : 'CANCELLED'}
+            </div>
+          </div>
+        );
+      }
+    },
+    {
+      field: 'action',
+      headerName: 'Action',
+      renderCell: (params) => (
+        <div className='flex h-full justify-center items-center'>
+          <button className='p-1 hover:bg-green-500 text-green-500 hover:text-white  rounded-full' onClick={() => handleShowOrderDetails(params.row.orderId)}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6" >
+              <path fillRule="evenodd" d="M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z" clipRule="evenodd" />
+              <path fillRule="evenodd" d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375ZM6 12a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V12Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 15a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V15Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 18a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V18Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+            </svg>
+          </button>
+        </div>    
+      )
+    }  
+  ]
   return (
     <div className='h-screen flex'>
       <LeftSideBar />
@@ -93,61 +145,28 @@ function Payment() {
         <div className='py-5 px-[30px] mx-auto'>
           <TopLayout text='Payment' />
           <div className='w-full flex justify-end items-center relative'>
-            <input type='text' className='p-2 border rounded-md mr-4' placeholder='Search' />
-            <div className='cursor-pointer'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='w-8 h-8 text-red-500'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75'
-                />
-              </svg>
-            </div>
+            
           </div>
-          <div className='overflow-x-auto mt-6'>
-            <table className='min-w-full border-spacing-x-1 border-gray-200'>
-              <thead>
-                <tr className='border-b'>
-                  <th className='py-3 px-2 text-center text-xs font-bold uppercase'>No</th>
-                  <th className='py-3 px-2 text-center text-xs font-bold uppercase'>Invoice Code</th>
-                  <th className='py-3 px-2 text-center text-xs font-bold uppercase'>Create Date</th>
-                  <th className='py-3 px-2 text-center text-xs font-bold uppercase'>Transaction Code</th>
-                  <th className='py-3 px-2 text-center text-xs font-bold uppercase'>Status</th>
-                  <th className='py-3 px-2 text-center text-xs font-bold uppercase'>Total Amount</th>               
-                  <th className='py-3 px-2 text-center text-xs font-bold uppercase'>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {payments.map((pay, index) => (
-                  <tr key={pay.id}>
-                    <td className='py-2 px-2 text-center border-b border-gray-200'>{index + 1}</td>
-                    <td className='py-2 px-2 text-center border-b border-gray-200'>{pay.invoiceCode}</td>                 
-                    <td className='py-2 px-2 text-center border-b border-gray-200'>{formatDateTime(pay.createDate)}</td>
-                    <td className='py-2 px-2 text-center border-b border-gray-200'>{pay.transactionCode}</td>
-                    <td className='py-2 px-2 text-center border-b border-gray-200'>{pay.status}</td>
-                    <td className='py-2 px-2 text-center border-b border-gray-200'>{formatCurrency(pay.amount)}</td>
-                    <td className='py-2 px-2 text-center border-b border-gray-200'>                     
-                      <div className='flex justify-center items-center'>
-                        <button className='p-1 hover:bg-green-500 text-green-500 hover:text-white  rounded-full' onClick={() => handleShowOrderDetails(pay.orderId)}>
-                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6" >
-                            <path fillRule="evenodd" d="M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z" clipRule="evenodd" />
-                            <path fillRule="evenodd" d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375ZM6 12a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V12Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 15a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V15Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 18a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V18Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
-                          </svg>
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+          <Paper sx={{ height: 670}}>
+              <DataGrid
+                rows={payments}
+                columns={columns}
+                pageSize={10}
+                pageSizeOptions={[5, 10, 20, 50, 100]}
+                rowHeight={60}
+                checkboxSelection
+                disableExtendRowFullWidth
+                getRowId={(row) => row.orderId}
+                sx={{
+                  '& .MuiDataGrid-columnHeaders': {
+                    backgroundColor: isDarkMode ? '#333' : '#f5f5f5'
+                  },
+                  '& .MuiDataGrid-row:hover': {
+                    backgroundColor: isDarkMode ? '#555' : '#e0e0e0'
+                  }
+                }}
+              />
+            </Paper>
               {isModalOpen && selectedOrder && (
                 <div className='fixed top-0 left-0 overflow-auto  w-full h-full text-gray-600 flex justify-center items-center bg-gray-800 z-50 bg-opacity-50'>
                   <div className='bg-white p-4 border rounded-lg'>
