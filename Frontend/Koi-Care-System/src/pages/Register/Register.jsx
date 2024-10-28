a909aeaf40e66809a9b67b2c96ecd7f1babaf3d8
VinhCanCode <sandilove59@gmail.com>
2024-10-28 18:51:29 +0700

    fix login and register page

--- Frontend/Koi-Care-System/src/pages/Register/Register.jsx
+++ Frontend/Koi-Care-System/src/pages/Register/Register.jsx
@@ -6,6 +6,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import axios from 'axios'
 import { FaSpinner } from 'react-icons/fa'
 import backgroundVideo from '../../assets/0917(1).mp4'
+import { TextField, InputAdornment } from '@mui/material'
 
 function Register() {
   const [showPassword, setShowPassword] = useState(false)
@@ -54,12 +55,12 @@ function Register() {
 
         <form className='relative' onSubmit={handleSubmit(onSubmit)} noValidate>
           <div className='mb-4'>
-            <div className='absolute -top-[12px] left-3 font-semibold bg-custom-Beige'>Email</div>
-            <input
-              type='email'
+            <TextField
               id='email'
+              type='email'
+              label='Email'
               placeholder='acd@gmail.com'
-              className='w-full p-3 bg-gray-700 bg-transparent border border-gray-500 placeholder-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 transition-colors duration-200'
+              variant='outlined'
               {...register('email', {
                 required: 'Email is required',
                 pattern: {
@@ -67,29 +68,32 @@ function Register() {
                   message: 'Invalid email format'
                 }
               })}
+              error={!!errors.email}
+              helperText={errors.email ? errors.email.message : ''}
+              className='w-full p-3 bg-gray-700 bg-transparent'
             />
-            {errors.email && <p className='text-red-500 text-sm'>{errors.email.message}</p>}
           </div>
 
           <div className='mb-4 relative'>
-            <div className='absolute -top-[12px] left-3 font-semibold bg-custom-Beige'>Username</div>
-            <input
-              type='text'
-              id='name'
+            <TextField
+              id='username'
+              label='Username'
               placeholder='Name'
-              className='w-full p-3 bg-gray-700 bg-transparent border border-gray-500 placeholder-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 transition-colors duration-200'
+              variant='outlined'
               {...register('username', { required: 'Name is required' })}
+              error={!!errors.username}
+              helperText={errors.username ? errors.username.message : ''}
+              className='w-full p-3 bg-gray-700 bg-transparent'
             />
-            {errors.username && <p className='text-red-500 text-sm'>{errors.username.message}</p>}
           </div>
 
           <div className='mb-4 relative'>
-            <div className='absolute -top-[12px] left-3 font-semibold bg-custom-Beige'>Password</div>
-            <input
-              type={showPassword ? 'text' : 'password'}
+            <TextField
               id='password'
+              type={showPassword ? 'text' : 'password'}
+              label='Password'
               placeholder='***********'
-              className='w-full p-3 bg-gray-700 bg-transparent border border-gray-500 placeholder-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 transition-colors duration-200'
+              variant='outlined'
               {...register('password', {
                 required: 'Password is required',
                 minLength: {
@@ -97,55 +101,85 @@ function Register() {
                   message: 'Password must be at least 8 characters'
                 }
               })}
+              error={!!errors.password}
+              helperText={errors.password ? errors.password.message : ''}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <span className='cursor-pointer text-gray-300' onClick={() => setShowPassword(!showPassword)}>
+                      {showPassword ? (
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          viewBox='0 0 24 24'
+                          fill='currentColor'
+                          className='size-6'
+                        >
+                          <path d='M12 4.5C5.25 4.5 1.5 12 1.5 12s3.75 7.5 10.5 7.5S22.5 12 22.5 12 18.75 4.5 12 4.5Zm0 12a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9Zm0-7.5a3 3 0 1 0 0 6 3 3 0 0 0 0-6Z' />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          viewBox='0 0 24 24'
+                          fill='currentColor'
+                          className='size-6'
+                        >
+                          <path d='M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z' />
+                          <path d='M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0 1 15.75 12ZM12.53 15.713l-4.243-4.244a3.75 3.75 0 0 0 4.244 4.243Z' />
+                          <path d='M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z' />
+                        </svg>
+                      )}
+                    </span>
+                  </InputAdornment>
+                )
+              }}
+              className='w-full p-3 bg-gray-700 bg-transparent'
             />
-            <span
-              className='absolute right-3 top-[0.7rem] cursor-pointer text-gray-300'
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              {showPassword ? (
-                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='size-6'>
-                  <path d='M12 4.5C5.25 4.5 1.5 12 1.5 12s3.75 7.5 10.5 7.5S22.5 12 22.5 12 18.75 4.5 12 4.5Zm0 12a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9Zm0-7.5a3 3 0 1 0 0 6 3 3 0 0 0 0-6Z' />
-                </svg>
-              ) : (
-                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='size-6'>
-                  <path d='M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z' />
-                  <path d='M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0 1 15.75 12ZM12.53 15.713l-4.243-4.244a3.75 3.75 0 0 0 4.244 4.243Z' />
-                  <path d='M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z' />
-                </svg>
-              )}
-            </span>
-            {errors.password && <p className='text-red-500 text-sm'>{errors.password.message}</p>}
           </div>
 
           <div className='mb-4 relative'>
-            <div className='absolute -top-[12px] left-3 font-semibold bg-custom-be'>Confirm Password</div>
-            <input
-              type={showPassword ? 'text' : 'password'}
+            <TextField
               id='confirm_password'
+              type={showPassword ? 'text' : 'password'}
+              label='Confirm Password'
               placeholder='***********'
-              className='w-full p-3 bg-gray-700 bg-transparent border border-gray-500 placeholder-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 transition-colors duration-200'
+              variant='outlined'
               {...register('confirm_password', {
                 required: 'Confirm password is required',
                 validate: (value) => value === password || 'Passwords do not match'
               })}
+              error={!!errors.confirm_password}
+              helperText={errors.confirm_password ? errors.confirm_password.message : ''}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <span className='cursor-pointer text-gray-300' onClick={() => setShowPassword(!showPassword)}>
+                      {showPassword ? (
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          viewBox='0 0 24 24'
+                          fill='currentColor'
+                          className='size-6'
+                        >
+                          <path d='M12 4.5C5.25 4.5 1.5 12 1.5 12s3.75 7.5 10.5 7.5S22.5 12 22.5 12 18.75 4.5 12 4.5Zm0 12a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9Zm0-7.5a3 3 0 1 0 0 6 3 3 0 0 0 0-6Z' />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          viewBox='0 0 24 24'
+                          fill='currentColor'
+                          className='size-6'
+                        >
+                          <path d='M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z' />
+                          <path d='M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0 1 15.75 12ZM12.53 15.713l-4.243-4.244a3.75 3.75 0 0 0 4.244 4.243Z' />
+                          <path d='M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z' />
+                        </svg>
+                      )}
+                    </span>
+                  </InputAdornment>
+                )
+              }}
+              className='w-full p-3 bg-gray-700 bg-transparent'
             />
-            <span
-              className='absolute right-3 top-[0.7rem] cursor-pointer text-gray-300'
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              {showPassword ? (
-                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='size-6'>
-                  <path d='M12 4.5C5.25 4.5 1.5 12 1.5 12s3.75 7.5 10.5 7.5S22.5 12 22.5 12 18.75 4.5 12 4.5Zm0 12a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9Zm0-7.5a3 3 0 1 0 0 6 3 3 0 0 0 0-6Z' />
-                </svg>
-              ) : (
-                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='size-6'>
-                  <path d='M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z' />
-                  <path d='M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0 1 15.75 12ZM12.53 15.713l-4.243-4.244a3.75 3.75 0 0 0 4.244 4.243Z' />
-                  <path d='M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z' />
-                </svg>
-              )}
-            </span>
-            {errors.confirm_password && <p className='text-red-500 text-sm'>{errors.confirm_password.message}</p>}
           </div>
 
           <button
